90489e9080bc083c1b86d52b5a71ff331160d0bb
Shani Ranasinghe <shani@wso2.com>
2023-06-13 11:08:41 +0530

    Create config.js

---
+++ web-apps/react-spa/public/config.js
@@ -0,0 +1,9 @@
+    ```
+    window.config = {
+    apiUrl: 'https://api.example.com',
+        featureFlags: {
+            enableNewFeature: true,
+            enableExperimentalFeature: false,
+        },
+    };
+    ```
